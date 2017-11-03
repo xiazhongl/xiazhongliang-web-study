4d7f3179812ea422d1db1cdc834abe25f39d73f1
xiazhongl name <l>
2017-11-03 16:15:00 +0800

    daima

---
+++ vs code hc/5/1.js
@@ -0,0 +1,15 @@
+<script type="text/javascript">
+    $("#header_head").scrollBanner({
+        images : [
+        {src:"img/banner1.png",title:"banner1",},
+        {src:"img/banner2.png",title:"banner2",},
+        {src:"img/banner3.png",title:"banner3",},
+        {src:"img/banner4.png",title:"banner4",}
+    ],
+    scrollTime:1000,
+    bannerHeight:"700px",
+    iconColor: "#FFFFFF",
+    iconHoverColor : "#82C900",
+    iconPosition : "center"
+});
+</script>
