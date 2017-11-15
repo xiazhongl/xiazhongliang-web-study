8ec4ba8488a08f1be11f807235a2ffbf8fce35ec
xiazhongl name <xiazhongliang10@163.com>
2017-11-15 21:30:56 +0800

    代码提交信息

--- vs code hc/xiangmu/xiangmu.js
+++ vs code hc/xiangmu/xiangmu.js
@@ -1,13 +1,1 @@
-$('#header_head').scrollBanner({
-  images: [
-    {src: 'img/292817.jpg',title: 'banner1'},
-    {src: 'img/292917.jpg',title: 'banner2'},
-    {src: 'img/banner3.png',title: 'banner3'},
-    {src: 'img/banner4.png',title: 'banner4'}
-  ],
-  scrollTime: 1000,
-  bannerHeight: '700px',
-  iconColor: '#FFFFFF',
-  iconHoverColor: '#82C900',
-  iconPosition: 'center'
-})
+
