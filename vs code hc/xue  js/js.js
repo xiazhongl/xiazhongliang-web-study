07b67e64c95ef71a61754ccf32dd3d8d059dbab0
xiazhongl name <xiazhongliang10@163.com>
2018-01-02 11:57:51 +0800

    1.2

--- vs code hc/xue  js/js.js
+++ vs code hc/xue  js/js.js
@@ -16,10 +16,93 @@ function c () {
   }
 }
 var momo = ['xia', 'zhong', 'liang']
-var tom = {
-  firstname="boll",
-  lastname: "Gates",
-  id: 5566
+var message = 'Hello world!'
+var x = message.toUpperCase()
+var xia = 10
+if (xia == 10) {
+  document.write('xia')
 }
-var message = "Hello world!"; 
-var x = message.toUpperCase();
+if (xia >= 10) {
+  document.write('xia')
+}else {
+  document.write('no xia')
+}
+if (xia < 10) {
+  document.write('no xia')
+}else if (xia < 20) {
+  document.write('xia')
+}else {
+  document.write('not xia')
+}
+
+function chen (a, b) {
+  return a * b
+}
+document.getElementById('qq').innerHTML = chen(3, 4)
+
+var qq = 24
+switch (qq) {
+  case 10:
+    x = '大'
+    break
+  case 1:
+    x = '一样'
+    break
+  case 3:
+    x = '小'
+    break
+  default:
+    x = '不是不是不是不是'
+}
+document.getElementById('ww').innerHTML = x
+
+carv = ['1', '2', '3', '4']
+for ( var i = 0;i < carv.length;i++) {
+  document.write(carv[i] + '</br>')
+}
+function ee () {var e = '', i = 0
+  while(i < 5){
+    x = x + i + '</br>'
+    i++
+  }document.getElementById('ee').innerHTML = x
+}
+
+cars = ['a', 'b', 'c', 'd']
+var i = 0
+for (;cars[i];) {
+  document.write(cars[i] + '</br>')
+  i++
+}
+cars = ['a', 'b', 'c', 'd']
+var i = 0
+while (cars[i]){
+  document.write(cars[i] + '</br>')
+  i++
+}
+function moy () {
+  var x = '',i = 0
+  for (i = 0;i < 10;i++) {
+    if (i == 3) break
+    x = x + i + '</br>'
+  }
+  document.getElementById('rr').innerHTML = x
+}
+function my () {
+  var x = '',i = 0
+  for (i = 0;i < 10;i++) {
+    if (i == 3) continue
+    x = x + i + '</br>'
+  }
+  document.getElementById('tt').innerHTML = x
+}
+document.getElementById('okj').style.color = '#ffffff'
+function ookkl (id) {
+  id.innerHTML = '谢谢！'
+}
+function mOver (obj) {
+  obj.innerHTML = '谢谢'
+}
+
+function mOut (obj) {
+  obj.innerHTML = '把鼠标移到上面'
+}
